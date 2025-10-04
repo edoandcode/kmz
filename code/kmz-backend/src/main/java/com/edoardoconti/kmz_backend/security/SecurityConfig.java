018bd7dd8c938e76cb56781baecaba885382aa54
Edoardo Conti <edoandcode@gmail.com>
2025-10-04 11:32:04 +0200

    code - kmz-backend - enabled CORS for http://localhost:3000

--- code/kmz-backend/src/main/java/com/edoardoconti/kmz_backend/security/SecurityConfig.java
+++ code/kmz-backend/src/main/java/com/edoardoconti/kmz_backend/security/SecurityConfig.java
@@ -22,6 +22,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -61,11 +66,14 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+
+
+
         http
                 // Stateless: no HTTP session will be created, JWT will be used instead
                 .sessionManagement(c ->
                         c.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // Disable CSRF as it is unnecessary for stateless REST APIs
                 .csrf(AbstractHttpConfigurer::disable)
 
@@ -213,6 +221,18 @@ public class SecurityConfig {
 
         // Return the built SecurityFilterChain
         return http.build();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of("http://localhost:3000")); // il tuo frontend
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowCredentials(true); // se usi cookie / auth
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
